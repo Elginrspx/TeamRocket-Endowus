25e35556224975a06de29aee287abb4ceb79ef8e
Elgin Seow <elgin.rspx@gmail.com>
2022-01-12 15:07:23 +0800

    Added 4 more Animations for total of 5 Animations
    Updated spritesheet

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -18,7 +18,6 @@ export default class Scene1 extends Phaser.Scene
         this.load.tilemapTiledJSON('tilemap', 'tilemaps/level-1.json')
 
         // Preload Character
-        //this.load.image('flatboy', 'characters/flatboy/Idle (10).png')
         this.load.atlas('flatboy', 'characters/flatboy/spritesheet.png', 'characters/flatboy/spritesheet.json')
     }
 
@@ -35,62 +34,276 @@ export default class Scene1 extends Phaser.Scene
         // Create Character
         this.flatboy = this.add.sprite(300, 300, 'flatboy')
 
-        /* Use these commands to get exact frame names for animations 
+        /* Use these commands to get exact frame names for animations
         var frameNames = this.textures.get('flatboy').getFrameNames();
         console.log(frameNames)
         */
 
+        // Create Animation for - Dead
+        this.anims.create({
+            key: 'dead',
+            frames: [{
+                key: 'flatboy',
+                frame: 'Dead (1).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (2).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (3).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (4).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (5).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (6).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (7).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (8).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (9).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (10).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (11).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (12).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (13).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (14).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Dead (15).png'
+            }, ],
+            frameRate: 15,
+            repeat: 0
+        })
+
+        // Create Animation for - Idle
         this.anims.create({
             key: 'idle',
             frames: [{
                 key: 'flatboy',
-                frame: 'Idle (1)_1.png'
+                frame: 'Idle (1).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (2)_1.png'
+                frame: 'Idle (2).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (3)_1.png'
+                frame: 'Idle (3).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (4)_1.png'
+                frame: 'Idle (4).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (5)_1.png'
+                frame: 'Idle (5).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (6)_1.png'
+                frame: 'Idle (6).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (7)_1.png'
+                frame: 'Idle (7).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (8)_1.png'
+                frame: 'Idle (8).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (9)_1.png'
+                frame: 'Idle (9).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (10)_1.png'
+                frame: 'Idle (10).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (11)_1.png'
+                frame: 'Idle (11).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (12)_1.png'
+                frame: 'Idle (12).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (13)_1.png'
+                frame: 'Idle (13).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (14)_1.png'
+                frame: 'Idle (14).png'
             }, {
                 key: 'flatboy',
-                frame: 'Idle (15)_1.png'
-            }],
-            frameRate: 8,
+                frame: 'Idle (15).png'
+            }, ],
+            frameRate: 15,
             repeat: -1
         })
-        this.flatboy.play("idle")
+
+        // Create Animation for - Jump
+        this.anims.create({
+            key: 'jump',
+            frames: [{
+                key: 'flatboy',
+                frame: 'Jump (1).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (2).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (3).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (4).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (5).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (6).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (7).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (8).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (9).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (10).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (11).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (12).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (13).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (14).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Jump (15).png'
+            }, ],
+            frameRate: 15,
+            repeat: -1
+        })
+
+        // Create Animation for - Run
+        this.anims.create({
+            key: 'run',
+            frames: [{
+                key: 'flatboy',
+                frame: 'Run (1).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (2).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (3).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (4).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (5).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (6).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (7).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (8).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (9).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (10).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (11).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (12).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (13).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (14).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Run (15).png'
+            }, ],
+            frameRate: 15,
+            repeat: -1
+        })
+
+        // Create Animation for - Walk
+        this.anims.create({
+            key: 'walk',
+            frames: [{
+                key: 'flatboy',
+                frame: 'Walk (1).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (2).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (3).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (4).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (5).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (6).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (7).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (8).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (9).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (10).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (11).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (12).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (13).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (14).png'
+            }, {
+                key: 'flatboy',
+                frame: 'Walk (15).png'
+            }, ],
+            frameRate: 15,
+            repeat: -1
+        })
+
+        this.flatboy.play("run")
     }
 }
