9eb666fd846e184919df078341dc9fc87ef457d0
Tananatham <73059344+Tananatham@users.noreply.github.com>
2022-04-05 14:13:18 +0800

    Update LoginScene.js

--- src/scenes/LoginScene.js
+++ src/scenes/LoginScene.js
@@ -25,24 +25,35 @@ export default class LoginScene extends Phaser.Scene
     async create()
     {
     
-        this.usernameInput = this.add.dom(640, 360).createFromCache("form").setPerspective(800)
+        this.usernameInput = this.add.dom(400, 300).createFromCache("form").setPerspective(800)
 
         this.returnKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
 
         this.returnKey.on("down", event => {
-            //let username = this.usernameInput.getChildByName("username");
-            //if(username.value != "") {
-                this.scene.start("Scene0", { });
-                //console.log("press enter")
-                //username: username.value 
-            //}
+            let username = this.usernameInput.getChildByName("username");
+            for (let i = 0; i < this.dataJSON.length; i++) {
+
+                if (this.dataJSON[i]["email"] == username.value) {
+                    console.log("Scene Started!")
+                    this.scene.start("Scene0", { })
+                    console.log(this.scene)
+                }
+
+                else if (this.dataJSON[i]["email"] != username.value) {
+                    console.log("Pls try again")
+                }
+
+              }
+
+            
+ 
         }) 
 
         //Calling Player's Persona from Game 1 Mongo DB
         try {
-            this.playerJSON = await fetch("http://localhost:3000/get")
+            this.dataJSON = await fetch("http://localhost:3000/get")
                 .then(response => response.json());
-        this.playerPersona = this.playerJSON[0]["lifestage"]
+        this.playerPersona = this.dataJSON[0]["lifestage"]
         } catch (e) {
             console.error(e);
         }
