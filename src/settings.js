e86b8d35c4fcc3a7e161d3131a7ba3d752663128
Tananatham <73059344+Tananatham@users.noreply.github.com>
2022-04-16 11:18:10 +0800

    update to married_man flow

--- src/settings.js
+++ src/settings.js
@@ -12,12 +12,13 @@ export const PersonaEvents = {
     "student": [1, 2, 13, 3, 4, 14, 5],
     "fresh_grad": [3, 4, 13, 5, 6, 14, 7],
     "bachelor": [4, 5, 13, 7, 8, 14, 9],
-    "married_man": [6, 7, 13, 8, 9, 14, 11],
+    "married_man_original": [6, 7, 13, 8, 9, 14, 11],
+    "married_man": [9, 16, 13, 8, 15, 7, 14],
     "family_man": [8, 9, 13, 10, 11, 14, 12]
 }
 
 export const SceneEventMapping = {
     "scene-1": [1, 4, 7, 10, 13],
-    "scene-2": [2, 5, 8, 11, 14],
-    "scene-3": [3, 6, 9, 12]
+    "scene-2": [2, 5, 8, 11, 14, 16],
+    "scene-3": [3, 6, 9, 12, 15]
 }
