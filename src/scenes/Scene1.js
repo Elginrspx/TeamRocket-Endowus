76ade76a8bdba5282d73af6eac0527bb922e6882
Tananatham <73059344+Tananatham@users.noreply.github.com>
2022-04-17 23:41:45 +0800

    update

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -126,27 +126,6 @@ export default class Scene1 extends Phaser.Scene
                 faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
             })
         }
-
-        
-        try {
-            // if (this.player.username && this.player.score) {
-            //     await fetch("http://localhost:3000/create", {
-            //         "method": "POST",
-            //         "headers": {
-            //             "content-type": "application/json"
-            //         },
-            //         "body": JSON.stringify(this.player)
-            //     });
-            // }
-    
-            this.playerPersona = await fetch("http://localhost:3000/get")
-                .then(response => response.json());
-            console.log(this,this.playerPersona)
-    
-        } catch (e) {
-            console.error(e);
-        }
-        
         
 
     }
